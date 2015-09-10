db778a1568a5f216d7192a670e01a82800a4e44c
Jay Wengrow <jaywngrw@gmail.com>
2015-09-10 17:40:02 -0500

    setting up mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,14 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port:    587,
+    domain:  'anyonecanhaveawebsite.com',
+    user_name: ENV["MAILER_USERNAME"],
+    password: ENV["MAILER_PASSWORD"]
+  }
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port:    587,
+    domain:  'anyonecanhaveawebsite.com',
+    user_name: ENV["MAILER_USERNAME"],
+    password: ENV["MAILER_PASSWORD"]
+  }
+  
 end
