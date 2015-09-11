174554412213fafb30a95c93561c4237f12d6d96
Jay Wengrow <jaywngrw@gmail.com>
2015-09-11 13:17:09 -0500

    text tweak

--- vendor/assets/javascripts/theme/contact_me.js
+++ vendor/assets/javascripts/theme/contact_me.js
@@ -33,7 +33,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thanks for reaching out! We'll get back to you shortly.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
