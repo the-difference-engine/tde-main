9e35455ceb3dcaa953a6a39c53f51a5617ed51e0
rymcmahon <toedragdangle9@gmail.com>
2016-06-22 16:00:39 -0500

    fixed navigation links in projects controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+
 //= require theme/moderniz.min
 //= require theme/jquery-migrate.min
 //= require theme/jquery.easing.1.3.min
