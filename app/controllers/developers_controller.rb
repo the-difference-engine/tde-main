12bdf0c88c21f1f6fc34342573ca5121bcddd617
Kimberly Lowe-Williams <kimberly@thedifferenceengine.io>
2016-11-28 15:43:07 -0600

    created developerpic form for uploading dev pics, new route, controller and model using paperclip

---
+++ app/controllers/developers_controller.rb
@@ -0,0 +1,47 @@
+class DevelopersController < ApplicationController
+	before_action :authenticate_admin!, :except => [:show]
+  def index
+    @developers = Developer.all
+  end
+
+  def show
+    @developer = Developer.find(params[:id])
+  end
+
+  def new
+    @developer = Developer.new
+  end
+
+  def edit
+    @developer = Developer.find(params[:id])
+  end
+
+  def create
+    @developer = Developer.new(developer_params)
+      if @developer.save
+        redirect_to @developer, notice: "New Developer Created!"
+      else
+        render 'new'
+      end
+  end
+
+  def update
+    @developer = Developer.find(params[:id])
+      if @developer.update(developer_params)
+        redirect_to @developer, notice: "Developer Updated!"
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @developer = Developer.find(params[:id])
+    @developer.destroy
+    redirect_to developers_path, alert: "Developer Destroyed!"
+  end
+
+  private
+    def developer_params
+      params.require(:developer).permit(:title, :first_name, :last_name, :developerpic)
+    end
+end
