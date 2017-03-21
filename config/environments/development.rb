f10cfe234fd18f2592ee44fa110f4d608ffd6257
rymcmahon <toedragdangle9@gmail.com>
2017-03-21 15:25:26 -0500

    moved s3 path to paperclip initializer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,9 +58,7 @@ Rails.application.configure do
     s3_credentials: {
     bucket: ENV['AWS_S3_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    Paperclip::Attachment.default_options[:url] = ':s3_domain_url',
-    Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
