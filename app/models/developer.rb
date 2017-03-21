fcfc9f14914eacb1bfac8f0789246dfc01891d01
rymcmahon <toedragdangle9@gmail.com>
2017-03-21 12:57:12 -0500

    changed s3 path and url and access key

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -2,4 +2,7 @@ class Developer < ActiveRecord::Base
 	validates :title, :first_name, :last_name, :developerpic, presence: true
 	has_attached_file :developerpic, styles: { medium: "600x400>" }
   	validates_attachment_content_type :developerpic, content_type: /\Aimage\/.*\z/
+
+	Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+  Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
