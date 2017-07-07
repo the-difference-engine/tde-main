ef4d090a1ab7a0b89bd7367a11c70a87794ff78c
Kimberly Lowe-Williams <kimberly@thedifferenceengine.io>
2017-07-07 18:01:31 -0500

    added Stripe controller and views

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,23 +4,35 @@ class ChargesController < ApplicationController
 	end
 
 	def create
-	  # Amount in cents
-	  @amount = 500
+	  @amount = params[:amount]
 
-	  customer = Stripe::Customer.create(
-	    :email => params[:stripeEmail],
-	    :source  => params[:stripeToken]
+	  @amount = @amount.gsub('$', '').gsub(',', '')
+
+	  begin
+	    @amount = Float(@amount).round(2)
+	  rescue
+	    flash[:error] = 'Charge not completed. Please enter a valid amount in USD ($).'
+	    redirect_to new_charge_path
+	    return
+	  end
+
+	  @amount = (@amount * 100).to_i # Must be an integer!
+
+	  if @amount < 500
+	    flash[:error] = 'Charge not completed. Donation amount must be at least $5.'
+	    redirect_to new_charge_path
+	    return
+	  end
+
+	  Stripe::Charge.create(
+	    :amount => @amount,
+	    :currency => 'usd',
+	    :source => params[:stripeToken],
+	    :description => 'Custom donation'
 	  )
 
-	  charge = Stripe::Charge.create(
-	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
-	    :currency    => 'usd'
-	  )
-
-	rescue Stripe::CardError => e
-	  flash[:error] = e.message
-	  redirect_to new_charge_path
+	  rescue Stripe::CardError => e
+	    flash[:error] = e.message
+	    redirect_to new_charge_path
 	end
 end
