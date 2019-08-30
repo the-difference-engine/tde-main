b2520865ef5de6335acc37275324e128c7a88f3c
Kimberly Lowe-Williams <klowewilliams@klowewillia-ltm.internal.salesforce.com>
2019-08-29 23:55:16 -0500

    added aws region

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,7 @@ Rails.application.configure do
     bucket: ENV['AWS_S3_BUCKET'],
     access_key_id: ENV['AWS_ACCESS_KEY_ID'],
     secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    s3_region: "us-west-2"
     }
   }
 end
