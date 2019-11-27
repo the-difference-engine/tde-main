50c73a2c5e60aafad1da1355f7de4da83e759576
Kimberly Lowe-Williams <klowewilliams@klowewillia-ltm.internal.salesforce.com>
2019-11-27 00:00:50 -0600

    removed changes to application controller enabled force_ssl from production file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  APP_DOMAIN = 'www.thedifferenceengine.io'
-  before_filter :ensure_domain
-
-  def ensure_domain
-  	unless request.env['HTTP_HOST'] == APP_DOMAIN || Rails.env.development?
-      redirect_to "https://#{APP_DOMAIN}", :status =&gt; 301
-  	end
-  end
-
   def after_sign_in_path_for(resource)
     projects_path
   end
