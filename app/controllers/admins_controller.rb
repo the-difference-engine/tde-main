f2a4c6e4056bfcbb55c574fa1a5e4b00eb84ba5c
Nick Harris <you@example.com>
2020-05-24 17:41:18 -0500

    Add further authorization for super-admins

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,19 @@
 class AdminsController < ApplicationController
-  before_action :authenticate_admin!, :except => [:show]
+  before_action :authenticate_admin!, :super_admin_only 
+  
   def index
     @admins = Admin.all
   end
+
+  private
+
+  def super_admin_only
+    unless current_admin.super_admin?
+      ## redirect_to :back, :alert => "Access denied."
+      render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+    end
+  end
+
+end 
+
+## render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
