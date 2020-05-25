df19b33c76acabeac18a1cf33504942fbeac13fe
Nick Harris <you@example.com>
2020-05-25 14:10:17 -0500

    Add Admin CRUD UI pages, clean up overall UI

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -5,6 +5,42 @@ class AdminsController < ApplicationController
     @admins = Admin.all
   end
 
+  def show
+    @admin = Admin.find(params[:id])
+  end
+
+  def new
+    @admin = Admin.new
+  end
+
+  def edit
+    @admin = Admin.find(params[:id])
+  end
+
+  def create
+    @admin = Admin.new(admin_params)
+      if @admin.save
+        redirect_to @admin, notice: "New Admin Created!"
+      else
+        render 'new'
+      end
+  end
+
+  def update
+    @admin = Admin.find(params[:id])
+      if @admin.update(admin_params)
+        redirect_to @admin, notice: "Admin Updated!"
+      else
+        render 'edit'
+      end
+  end
+
+  def destroy
+    @admin = Admin.find(params[:id])
+    @admin.destroy
+    redirect_to admins_path, alert: "Admin Destroyed!"
+  end
+
   private
 
   def super_admin_only
@@ -14,6 +50,8 @@ class AdminsController < ApplicationController
     end
   end
 
-end 
+  def admin_params
+    params.require(:admin).permit(:first_name, :last_name, :email, :password, :super_admin)
+  end
 
-## render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
+end 
